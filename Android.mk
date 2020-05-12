a77f4fe6682b07ec7ff5ce6cc01a4f95b8d14f70
lindwurm <lindwurm.q@gmail.com>
2020-05-12 14:52:45 +0900

    OmniSwitch: Add privapp permissions xml
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- Android.mk
+++ Android.mk
@@ -25,4 +25,14 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVATE_PLATFORM_APIS := true
 #LOCAL_DEX_PREOPT := false
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.omnirom.omniswitch.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.omnirom.omniswitch.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
